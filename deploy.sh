ecca35396e3c7faff71b0c663e376e22edbd26d8
obozhinov <olya.bozhinov@gmail.com>
2020-06-16 14:38:12 +0300

    Deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t obozhinov/multi-client:latest -t obozhinov/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t obozhinov/multi-server:latest -t obozhinov/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t obozhinov/multi-worker:latest -t obozhinov/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push obozhinov/multi-client:latest
+docker push obozhinov/multi-server:latest
+docker push obozhinov/multi-woker:latest
+
+docker push obozhinov/multi-client:$SHA
+docker push obozhinov/multi-server:$SHA
+docker push obozhinov/multi-woker:$SHA
+
+kubectl apply -f k8s
+kubectl set image obozhinov/server-deployment server=obozhinov/multi-server:$SHA
+kubectl set image obozhinov/client-deployment server=obozhinov/multi-client:$SHA
+kubectl set image obozhinov/worker-deployment server=obozhinov/multi-worker:$SHA
