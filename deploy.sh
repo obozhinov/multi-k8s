b8ddc5f872419477edb40bc85db14aa7da6c2e0e
obozhinov <olya.bozhinov@gmail.com>
2020-06-16 18:02:19 +0300

    Travis yml change

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ docker build -t obozhinov/multi-server:latest -t obozhinov/multi-server:$SHA -f 
 docker build -t obozhinov/multi-worker:latest -t obozhinov/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push obozhinov/multi-client:latest
 docker push obozhinov/multi-server:latest
-docker push obozhinov/multi-woker:latest
+docker push obozhinov/multi-worker:latest
 
 docker push obozhinov/multi-client:$SHA
 docker push obozhinov/multi-server:$SHA
-docker push obozhinov/multi-woker:$SHA
+docker push obozhinov/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image obozhinov/server-deployment server=obozhinov/multi-server:$SHA
